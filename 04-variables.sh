e0078083302344053e0fa384c2e566ce05ab6e2a
msivakumarreddy <info@joindevops.com>
2025-05-15 08:31:27 +0530

    hello world started

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Modi #there should be no space before and after =
-PERSON2=Musk
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: Hey $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
