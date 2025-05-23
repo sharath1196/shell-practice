ea840b054a4e4b2a4b408429786fae55964a103e
msivakumarreddy <info@joindevops.com>
2025-05-23 07:58:16 +0530

    hello world started

--- 20-backup.sh
+++ 20-backup.sh
@@ -62,7 +62,7 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
 if [ ! -z $FILES ]
 then
-    echo "Files found"
+    echo "Files to zip are: $FILES"
 else
     echo -e "No log files found older than 14 days ... $Y SKIPPING $N"
 fi
