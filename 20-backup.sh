dbc88b2f7d2afd23a457a24e714a63ea85c7bca7
msivakumarreddy <info@joindevops.com>
2025-05-23 08:32:47 +0530

    hello world started

--- 20-backup.sh
+++ 20-backup.sh
@@ -27,10 +27,10 @@ VALIDATE(){
 check_root(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+        echo -e "$R ERROR:: Please run this script with root access $N"
         exit 1 #give other than 0 upto 127
     else
-        echo "You are running with root access" | tee -a $LOG_FILE
+        echo "You are running with root access" 
     fi
 }
 
