9244d55c8584ff9c4683c5b9f7da674992a18e82
msivakumarreddy <info@joindevops.com>
2025-05-26 08:21:43 +0530

    hello world started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -11,7 +11,7 @@ do
     PARTITION=$(echo $line | awk '{print $7F}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="High Disk Usage on $PARTITION: $USAGE \n"
+        MSG+="High Disk Usage on $PARTITION: $USAGE <br>" #<br> represents HTML new
     fi
 done <<< $DISK_USAGE
 
