e58365b05d3b67e02dac6f1511a2e5b159fcc57d
msivakumarreddy <info@joindevops.com>
2025-05-26 08:27:34 +0530

    hello world started

--- 22-cpu-usage.sh
+++ 22-cpu-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  cpu_usage=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+  cpu_usage=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f%%\n", prefix, 100 - v }')
   echo "CPU Usage: ${cpu_usage}%"
   sleep 1
 done
