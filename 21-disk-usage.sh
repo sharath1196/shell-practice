aa95d602ede73428e8ccc10b41ea7f2c389a79e0
msivakumarreddy <info@joindevops.com>
2025-05-26 08:17:11 +0530

    hello world started

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -3,6 +3,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # in project it will be 75
 MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read line
 do
@@ -14,4 +15,6 @@ do
     fi
 done <<< $DISK_USAGE
 
-echo -e $MSG
+#echo -e $MSG
+
+sh mail.sh "DevOps Team" "High Disk Usage" $IP $MSG "info@joindevops.com" "ALERT-High Disk Usage"
