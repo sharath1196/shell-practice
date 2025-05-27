cd8d6b5523660ff4dc800d4bdcae247200ede91d
sharath1196 <sharat.batt07@gmail.com>
2025-05-27 17:12:16 +0530

    First commit

--- 05-variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Enter your pin number::"
-
-read -s PIN #here PIN is variable
-
-echo "Your number is: $PIN"
--- 17-set.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Hi, Good Morning"
-echoooooo "Hello, this will be error"
-echo "Hello, Morning"
