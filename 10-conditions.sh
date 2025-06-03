473272cd1b462b99eebf6b534a7e5458eeb59241
sharath1196 <sharat.batt07@gmail.com>
2025-06-03 20:20:07 +0530

    Added new tasks directory

---
+++ 10-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $1 -eq 20 ]
+then
+    echo "The entered argument is equal to 20"
+elif [ $1 -gt 20 ]
+then
+    echo "The entered argument is greater than 20"
+else
+    echo "The entered argument is less than 20"
+fi
+
---
+++ tasks/task1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Write a script to monitor:
+# CPU usage
+# Memory usage
+# Disk usage#
+
+date
+
+free -h
+uptime
+echo "$?"
+
+free -h
+
+date
