be5a0ae334373d912c1aee47bb53fd886f282698
sharath1196 <sharat.batt07@gmail.com>
2025-06-04 22:51:41 +0530

    Changes made

--- 11-install.sh
+++ 11-install.sh
@@ -7,19 +7,25 @@ if [ $USERID -eq 0 ]
 then
     echo "Running as Root"
     echo "Installing MySql"
+    dnf list installed mysql 
+    if [ $? -eq 0 ]
+    then
+        echo "MySql is already installed"
+    else
+        dnf install mysql -y
+    fi
 else
     echo "User ID : $USERID" 
     echo "ERROR:: Not running as Root User" 
     exit 1
 fi
 
-dnf install mysql -y
 
 if [ $? -eq 0 ]
 then
-    echo "Installing MySql is....SUCCESS"
+    echo "Installing MySql is.... SUCCESS"
 else
-    echo "Installing MySql is...FAILURE"
+    echo "Installing MySql is... FAILURE"
     exit 1
 fi
 
