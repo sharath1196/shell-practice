90773dae0dd3b0086b8787d87dc1b55619b5ee01
sharath1196 <sharat.batt07@gmail.com>
2025-06-05 00:30:44 +0530

    Changes made

--- 12-functions.sh
+++ 12-functions.sh
@@ -4,4 +4,40 @@ get_user_id(){
     id -u
 }
 
-get_user_id
+is_root_user(){
+    [ "$(get_user_id)" -eq 0 ]
+}
+
+is_package_installed(){
+    dnf list installed $1 &> /dev/null
+}
+
+install_package(){
+    dnf install $1 -y
+}
+
+log_success(){
+    echo "Installing $1 is......Success"
+}
+
+log_failure(){
+    echo "Installing $1 is......Failure"
+}
+
+main(){
+    if is_root_user;
+    then
+        if is_package_installed; 
+        then
+            echo "$1 already installed"
+        else
+            install_package && log_success || log_failure
+        fi
+    else
+        echo "User ID : $(get_user_id)"
+        echo "Not running as a root user"
+        log_failure;
+        exit 1
+    fi
+
+}
