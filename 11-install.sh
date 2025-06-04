9fcf348e66ca65252121201744c40f225d9c5a9c
sharath1196 <sharat.batt07@gmail.com>
2025-06-04 22:15:28 +0530

    changes made

--- 11-install.sh
+++ 11-install.sh
@@ -4,3 +4,11 @@ USERID=$(id -u)
 
 echo "$USERID"
 
+if [ $USERID -eq 0]
+then
+    echo "Running as Root"
+    echo "Installing MySql"
+    dnf install mysql -y
+else
+    echo "Not running as Root User"
+
