ef7a380375dd4cde589114220d491e29fd4d5111
sharath1196 <sharat.batt07@gmail.com>
2025-06-06 17:31:15 +0530

    made changes

---
+++ 14-functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+RESET="\e[0m"
+
+echo -e "Printing color $R RED $RESET"
+echo -e "Printing color $G GREEN $RESET"
+echo -e "Printing color $Y YELLOW $RESET"
