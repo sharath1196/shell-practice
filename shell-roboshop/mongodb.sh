8fad46f15efcdfe9a78f47aea3a1d7b9319c5899
sharath1196 <sharat.batt07@gmail.com>
2025-06-15 19:51:20 +0530

    made some changes

--- shell-roboshop/mongodb.sh
+++ shell-roboshop/mongodb.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+
+
+# Check if you are running as root 
+# if running as root 
+# install mongodb
+#     cp mongodb.repo to /etc/mongo.conf/mongodb.repo
+#     dnf install mongodb
+    
+#     change port 127.0.0.0 to 0.0.0.0 to allow access to all
+
+
+
 R='\e[31m'
 G='\e[32m'
 Y='\e[33m'
@@ -9,3 +22,21 @@ RESET='\e[0m'
 
 USERID=$(id -u)
 echo $USERID
+
+if [ $USERID -eq 0 ]
+then
+    cp mongodb.repo /etc/mongodb/mongo.conf
+    dnf install mongodb
+    sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongodb/mongo.conf
+    systemctl daemon-reload
+    systemctl enable mongodb
+    systemctl start mongodb
+else
+    echo "Not running as root user"
+
+
+
+
+
+
+
