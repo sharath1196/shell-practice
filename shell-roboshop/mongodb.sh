43b90ec56db30f054810b909b44091216102911d
sharath1196 <sharat.batt07@gmail.com>
2025-07-26 10:15:57 +0530

    made changes

--- shell-roboshop/mongodb.sh
+++ shell-roboshop/mongodb.sh
@@ -1,53 +1,51 @@
 #!/bin/bash
 
-
-
-
-# Check if you are running as root 
-# if running as root 
-# install mongodb
-#     cp mongodb.repo to /etc/mongo.conf/mongodb.repo
-#     dnf install mongodb
-    
-#     change port 127.0.0.0 to 0.0.0.0 to allow access to all
-
-
-
-R='\e[31m'
-G='\e[32m'
-Y='\e[33m'
-
-RESET='\e[0m'
-
-
 USERID=$(id -u)
-echo $USERID
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-if [ $USERID -eq 0 ]
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
 then
-    cp mongodb.repo /etc/yum.repos.d/mongo.repo
-
-    dnf install mongodb-org -y
-
-    sed -i 's/127.0.0.0/0.0.0.0/g' /etc/yum.repos.d/mongo.repo
-
-    systemctl daemon-reload
-
-    systemctl enable mongodb
-
-    systemctl start mongodb
-
-    systemctl restart mongodb
-
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-
-    echo "Not running as root user"
-
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "Copying MongoDB repo"
 
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb server"
 
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling MongoDB"
 
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Starting MongoDB"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Editing MongoDB conf file for remote connections"
 
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB"
