fd71c49ffd383640684b20c1ff7a4051627741ec
sharath1196 <sharat.batt07@gmail.com>
2025-07-29 04:57:42 +0530

    changes made

---
+++ shell-roboshop/catalogue.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enabling required nodejs"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Adding a user 'roboshop'"
+
+mkdir /app
+VALIDATE $? "Making a home directory of roboshop user" 
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+VALIDATE $? "Downloading the artifact"
+
+cd /app 
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "Extracting the artifact files here"
+
+cd /app 
+npm install
+VALIDATE $? "Build the artifact" 
